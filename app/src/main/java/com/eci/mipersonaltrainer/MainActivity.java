0d11c716f8a44b8040dcde14ccfaa2b50f5b7c69
Eliseo Cohen Imach <eliseoimach@gmail.com>
2015-05-02 19:54:20 -0300

    Commit Inicial

---
+++ app/src/main/java/com/eci/mipersonaltrainer/MainActivity.java
@@ -0,0 +1,55 @@
+package com.eci.mipersonaltrainer;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+    private TextView tv1;
+    private EditText et1,et2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=(EditText)findViewById(R.id.editText);
+        et2=(EditText)findViewById(R.id.editText2);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void sumar(View view) {
+        String valor1=et1.getText().toString();
+        String valor2=et2.getText().toString();
+        int nro1=Integer.parseInt(valor1);
+        int nro2=Integer.parseInt(valor2);
+        int suma=nro1+nro2;
+        Toast toast = Toast.makeText(this, "El resultado es " + suma, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
